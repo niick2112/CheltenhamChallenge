a07073dc8e13ef7f5cc0eb10c4f3075d7eccae76
Nick Bradley <niick2112@gmail.com>
2022-03-01 10:15:12 +0000

    Initial updates for 2022

--- src/index.js
+++ src/index.js
@@ -97,11 +97,12 @@ function loadRace(values) {
 
 function loadDayResults(values,date,form) {
 
-  var raceDay = new Date(2021, 2, date, 13, 00, 00);
+  var raceDay = new Date(2022, 2, date, 13, 00, 00);
   var currentTime = new Date();
   console.log(raceDay);
   if (currentTime < raceDay) {
-    $("#day-table").append("<p>Scores will be available from 1pm. Submit your entry <a href='" + form + "' target='blank'>here</a>.</p>")
+        //$("#day-table").append("<p>Scores will be available from 1pm. Submit your entry <a href='" + form + "' target='blank'>here</a>.</p>")
+        $("#day-table").append("<p>Coming soon.</p>")
   }
   else {
   var headings = values[0];
